1ca670c8d8e6e5173163cef73c1e5ce58b3f54e3
Davi Costa <davispcosta@gmail.com>
2020-08-04 14:58:50 -0300

    mockups done

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import NotFound from './containers/404';
 import Home from './containers/home';
 import Invite from './containers/invite';
+import Register from './containers/register';
+import Login from './containers/login';
 
 export default () => {
     return (
         <BrowserRouter basename="/">
             <Switch>
                 <Route path="/" exact={true} component={Home} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
                 <Route path="/invite" component={Invite} />
                 <Route path="*" component={NotFound}></Route>
             </Switch>
