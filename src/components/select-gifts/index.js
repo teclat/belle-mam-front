72769d07a869d13d4d03c6f35e1db84a0d028902
MeBSilva <38053552+MeBSilva@users.noreply.github.com>
2021-03-31 22:22:44 -0300

    Select Gifts component changes
    Styling Fixes and additions;
    Added Wordpress E-commerce API integration;
    Added product paging.

--- src/components/select-gifts/index.js
+++ src/components/select-gifts/index.js
@@ -1,97 +1,156 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import "./style.scss";
-import Product from '../product';
-import { Constants } from '../../constants';
+import Product from "../product";
+import { Constants } from "../../constants";
 import axios from "axios";
-import { Button } from 'antd';
+import { Button } from "antd";
 export default class SelectGifts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1,
+      products: [],
+      selecteds: [],
+      isLoading: false,
+      hasMorePages: true,
+    };
+    this.get();
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            products: [],
-            selecteds: []
+  get = async () => {
+    console.log(this.state.isLoading);
+    this.setState({
+      isLoading: true,
+    });
+
+    let user = JSON.parse(await localStorage.getItem("user"));
+    console.log("user", user);
+
+    axios
+      .get(Constants.ApiUrl + "products", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+
+        params: {
+          page: this.state.page,
+          per_page: 8,
+        },
+      })
+      .then((response) => {
+        let pageLimit = response.headers["x-wp-totalpages"];
+        if (pageLimit <= this.state.page) {
+          this.setState({
+            hasMorePages: false,
+          });
+          console.log("End of products list.");
+          console.log(this.state.hasMorePages);
         }
-        this.get();
-    }
 
-    get = async () => {
-        let user = JSON.parse(await localStorage.getItem("user"));
+        console.log(pageLimit);
 
-        axios.get(Constants.ApiUrl + 'products', {
-            headers: {
-                'Authorization': `Bearer ${user.token}`
-            }
-        })
-            .then((response) => {
-                this.setState({
-                    products: response.data
-                })
+        this.setState({
+          products: [...this.state.products, ...response.data],
+        });
+
+        console.log("PRODUCTS", this.state.products);
+
+        this.setState({ page: this.state.page + 1 });
+
+        this.setState({
+          isLoading: false,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({
+          isLoading: false,
+        });
+      });
+  };
+
+  change = (id, values) => {
+    console.log(values);
+    let selecteds = this.state.selecteds;
+    let exist = false;
+    if (selecteds.length > 0) {
+      let product = selecteds.filter((p) => p.product_id == id)[0];
+      if (product) {
+        exist = true;
+        if (values.selected == true) {
+          product.quantity = values.qtd;
+        } else {
+          var index = selecteds
+            .map((p) => {
+              return p.product_id;
             })
-            .catch((error) => {
-                console.log(error);
-            })
-    }
-
-    change = (id, values) => {
-        console.log(values)
-        let selecteds = this.state.selecteds;
-        let exist = false;
-        if (selecteds.length > 0) {
-            let product = selecteds.filter(p => p.product_id == id)[0];
-            if (product) {
-                exist = true;
-                if (values.selected == true) {
-                    product.quantity = values.qtd;
-                } else {
-                    var index = selecteds.map(p => {
-                        return p.product_id;
-                    }).indexOf(id);
-                    selecteds.splice(index, 1);
-                }
-            }
+            .indexOf(id);
+          selecteds.splice(index, 1);
         }
-
-        if (!exist) {
-            if (values.selected == true) {
-                selecteds.push({
-                    product_id: id,
-                    quantity: values.qtd
-                })
-            }
-        }
-        console.log("pppp", selecteds);
-        this.setState({ selecteds: selecteds })
+      }
     }
 
-    render() {
-        return (
-            <div id="select-gifts">
-                <div className="d-flex flex-column justify-content-center align-items-center title-box">
-                    <div className="d-flex">
-                        <h2>Falta pouco!</h2>
-                        <img src={require("../../assets/images/purple-heart.png")} />
-                    </div>
-                    <h4>Vamos escolher os produtos do seu evento</h4>
-                </div>
-                <div className="gifts justify-content-center">
-                    <ul class="d-flex flex-row justify-content-around">
-                        {
-                            this.state.products.map((product) => {
-                                return <Product event_product={product.id} change={this.change}
-                                    gifted={false} product={product} />
-                            })
-                        }
-                    </ul>
-                </div>
-                <div className="d-flex btns justify-content-center">
-                    <Button
-                        loading={this.props.loading}
-                        onClick={() => { this.props.save(this.state.selecteds) }} className="btn btn-secondary">
-                        FINALIZAR
-                    </Button>
-                </div>
-            </div>
-        )
+    if (!exist) {
+      if (values.selected == true) {
+        selecteds.push({
+          product_id: id,
+          quantity: values.qtd,
+        });
+      }
     }
+    console.log("pppp", selecteds);
+    this.setState({ selecteds: selecteds });
+  };
+
+  render() {
+    return (
+      <div id="select-gifts">
+        <div className="d-flex flex-column justify-content-center align-items-center title-box">
+          <div className="d-flex">
+            <h2>Falta pouco!</h2>
+            <img src={require("../../assets/images/purple-heart.png")} />
+          </div>
+          <h4>Vamos escolher os produtos do seu evento</h4>
+        </div>
+        <div className="gifts">
+          {/* <ul class="d-flex flex-row justify-content-around"> */}
+          <ul>
+            {this.state.products.map((product) => {
+              return (
+                <Product
+                  event_product={product.id}
+                  change={this.change}
+                  gifted={false}
+                  product={product}
+                />
+              );
+            })}
+          </ul>
+        </div>
+        {/* <div className="d-flex btns justify-content-around"> */}
+        <div className="btnsContainer">
+          {this.state.hasMorePages === true ? (
+            <Button
+              loading={this.state.isLoading}
+              onClick={() => {
+                this.get();
+              }}
+              className="btn btn-secondary"
+            >
+              MAIS PRODUTOS
+            </Button>
+          ) : null}
+          <Button
+            loading={this.props.isLoading}
+            onClick={() => {
+              this.props.save(this.state.selecteds);
+            }}
+            className="btn btn-secondary"
+          >
+            FINALIZAR
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
