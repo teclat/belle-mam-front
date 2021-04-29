c7fd758d62a7d923ee3b09243e55227919cccffe
MeBSilva <38053552+MeBSilva@users.noreply.github.com>
2021-04-29 08:04:03 -0300

    Guest gift selection and checkout Implementation
    Added GuestGiftList component;
    Added add to cart option in product lists;
    Added route to selected events gft lists;

--- src/components/client-dashboard/events/index.js
+++ src/components/client-dashboard/events/index.js
@@ -1,47 +1,45 @@
 import React, { Component, useState } from "react";
 import "./style.scss";
-import { Card, Modal } from "antd";
+import { Button, Card, Modal } from "antd";
 import { Constants } from "../../../constants";
 import axios from "axios";
+import { Link, Route, Switch } from "react-router-dom";
+import PersonalClient from "../personal";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+import {
+  getEventsRequest,
+  selectEventAction,
+} from "../../../redux/actions/eventsActions";
 
 function EventsGuest(props) {
-  const [events, setEvents] = useState([]);
-  const [isLoading, setIsloading] = useState(false);
+  const { isLoading, events, err } = useSelector((state) => ({
+    isLoading: state.event.isLoading,
+    events: state.event.events,
+    err: state.event.err,
+  }));
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
-    get();
+    getEvents();
   }, []);
 
-  const get = async () => {
+  const getEvents = async () => {
     let user = await JSON.parse(localStorage.getItem("user"));
-    //this.setState({ loading: true });
-    setIsloading(true);
+    dispatch(getEventsRequest(user));
+  };
 
-    axios
-      .post(
-        Constants.ApiUrl + "users/subscribed-events",
-        {
-          user_id: user.id,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${user.token}`,
-          },
-        }
-      )
-      .then((response) => {
-        //this.setState({ events: response.data, loading: false })
-        setEvents(response.data);
-        setIsloading(false);
-      })
-      .catch((error) => {
-        //this.setState({ loading: false });
-        setIsloading(false);
-        Modal.error({
-          content: "Erro de buscar eventos.",
-        });
-        console.log(error);
+  React.useEffect(() => {
+    if (err.length !== 0) {
+      Modal.error({
+        content: `Erro ao buscar eventos: ${err}`,
       });
+    }
+  }, [err]);
+
+  const selectEvent = (event) => {
+    dispatch(selectEventAction(event.event_id));
   };
 
   return (
@@ -50,15 +48,28 @@ function EventsGuest(props) {
         <h4>Eventos</h4>
       </div>
       <div>
+        {/* {isLoading !== false ? <LoadingComponent></LoadingComponent> : null} */}
         {events.length === 0 ? <h3>Não participa de eventos ainda.</h3> : null}
         {events.map((event) => {
           return (
             <div key={event.id} className={"mb-3"}>
               <Card>
                 <div className={"d-flex"}>
-                  <img src={event.event.baby_image_url} alt="" />
+                  {event.event.baby_image_url !== "" ? (
+                    <img src={event.event.baby_image_url} alt="" />
+                  ) : (
+                    <img
+                      src={require("../../../assets/images/woocommerce-placeholder.png")}
+                    />
+                  )}
                   <div>
-                    <h3>{event.event.baby_name}</h3>
+                    <Link
+                      className="event-button"
+                      onClick={() => selectEvent(event)}
+                      to={`/guest/event/${event.event.url}`}
+                    >
+                      {event.event.baby_name}
+                    </Link>
                     <p>{event.event.history_text}</p>
                     <p>{event.event.address}</p>
                     <p>{event.event.date}</p>
