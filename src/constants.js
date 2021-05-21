6ef3e9f381640cf988a16f3aeebb937f9c6ff643
MeBSilva <38053552+MeBSilva@users.noreply.github.com>
2021-05-21 09:10:04 -0300

    Production minor fixes
    -Updated backend api url;
    -Updated app Title;

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
 export const Constants = {
-  ApiUrl: "https://bellemam-dev-backend.herokuapp.com/api/",
+  //ApiUrl: "https://bellemam-dev-backend.herokuapp.com/api/",
   WooUrl: "https://bellemam.com.br/",
-  //ApiUrl: "https://belle-mam-back.herokuapp.com/api/",
+  ApiUrl: "https://belle-mam-back.herokuapp.com/api/",
   //ApiUrl: "http://localhost:8080/api/",
 };
+//https://belleman-back.herokuapp.com/api
+//https://belleman-gift.herokuapp.com/
