029e61680146bba24f95c2e6374b595ae5c92f0c
MeBSilva <38053552+MeBSilva@users.noreply.github.com>
2021-05-24 12:00:38 -0300

    Backend Url Fix
    -Fixed typo on ApiUrl constant.

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const Constants = {
   //ApiUrl: "https://bellemam-dev-backend.herokuapp.com/api/",
   WooUrl: "https://bellemam.com.br/",
-  ApiUrl: "https://belle-mam-back.herokuapp.com/api/",
+  ApiUrl: "https://belleman-back.herokuapp.com/api/",
   //ApiUrl: "http://localhost:8080/api/",
 };
 //https://belleman-back.herokuapp.com/api
