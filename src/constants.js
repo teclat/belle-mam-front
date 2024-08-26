d44af7dd4c8c0bac4b4c33ea7859fb73c8feeccb
Raphael3c <phael.ufc@gmail.com>
2024-08-26 03:36:11 -0300

    Fix: API URL

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const Constants = {
-  ApiUrl: "https://bellemam-dev-backend.herokuapp.com/api/",
+  ApiUrl: "https://bellemam-backend.mg8.andreconjo.com/api/",
   WooUrl: "https://bellemam.com.br/",
   //ApiUrl: "https://belle-mam-back.herokuapp.com/api/",
   //ApiUrl: "http://localhost:8080/api/",
